13763bd783b6cb37cd1614bee5dba9dee72b0b20
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-05 11:32:55 -0700

    Update navbar.js

--- client/scripts/navbar.js
+++ client/scripts/navbar.js
@@ -1,3 +1,4 @@
+
 /*
   Why can't we just implement the navbar as a js file lol
 */
@@ -5,47 +6,46 @@ document.addEventListener("DOMContentLoaded", async function() {
   const style = document.createElement("style");
   style.textContent = `
     .topnav {
-      overflow: hidden;
+  overflow: hidden;
       background-color: #333;
       color: #000;
       height: 2.5em;
       font-size: 1em;
       display: flex;
       align-items: center;
-      justify-content: space-between;
+      justify-content: center;
       width: 100%;
       box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
       position: fixed;
       top: 0;
+      left: 0;
       z-index: 1000;
-      padding: 0 1em;
     }
-    .nav-links {
-      display: flex;
-      align-items: center;
-    }
-    .nav-links a {
+    .topnav a {
+      float: left;
+      display: block;
       color: #f2f2f2;
       text-align: center;
       padding: 14px 16px;
       text-decoration: none;
     }
-    .nav-links a:hover {
+    .topnav a:hover {
       background-color: #ddd;
       color: black;
     }
     .signedin {
+      position: fixed;
+      right: 3px;
+      top:3px;
       height: calc(2.5em - 7px);
       border: 1px solid #ddd;
       border-radius: 10px;
+      z-index: 1001;
       color: #f2f2f2;
       background-color: #333;
       text-align: center;
       text-decoration: none;
       display: flex;
-      align-items: center;
-      padding: 0 10px;
-      margin-left: auto;
     }
     .signedin:hover {
       background-color: #ddd;
@@ -55,32 +55,33 @@ document.addEventListener("DOMContentLoaded", async function() {
       width: 2em;
       height: 2em;
       border-radius: 50%;
-      margin-right: 10px;
+      padding: 0px;
+      margin: 0px;
+      display: inline-flex;
     }
     .signedin-username {
-      margin: 0;
-      display: flex;
-      align-items: center;
+      padding: 0px;
+      margin: 7px;
+      display: inline-flex;
     }
     #block {
       height: 2.5em;
-      width: 100%;
+	  width: 100%;
     }
+.
   `;
   document.head.append(style);
 
   var navbarHtml = `
   <div class="topnav">
-    <div class="nav-links">
-      <a href="/">Home</a>
-      <a href="/chat">Chat</a>
-      <a href="/search">Gallery</a>
-    </div>
-    <div id="block"></div>
+    <a href="/">Home</a>
+    <a href="/chat">Chat</a>
+    <a href="/search">Project Gallery</a>
   </div>
+  <div id="block"></div>
   `;
   var auth = await getAuth();
-  if (auth.auth) {
+  if (auth.user) {
     navbarHtml += `
     <a class="signedin" href="/profile">
       <img class="signedin-avatar" src="${auth.user.avatar}">
@@ -89,5 +90,8 @@ document.addEventListener("DOMContentLoaded", async function() {
     `;
   }
 
-  document.body.insertAdjacentHTML('afterbegin', navbarHtml);
+  const navbarContainer = document.createElement("div");
+  navbarContainer.innerHTML = navbarHtml;
+
+  document.body.prepend(navbarContainer);
 });
