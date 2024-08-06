d7723a0b4f4cc61dde1ea5360ce1d86dbf1a0b3a
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-05 19:07:14 -0700

    Update route.js

--- Project/route.js
+++ Project/route.js
@@ -1,21 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { publish, list, data, update, deleteProject, favorite, unfavorite, feature, unfeature, comment, editComment, deleteComment } = require("./project");
+const ProjectAPI = new (require("./project"))();
 const { adminAuth, userAuth } = require("../middleware/auth");
 
-router.route("/publish").post(userAuth, publish);
-router.route("/list").get(list);
-router.route("/data/:id").get(data);
-router.route("/update/:id").post(userAuth, update);
-router.route("/delete/:id").delete(userAuth, deleteProject);
-router.route("/delete/:id").get(userAuth, deleteProject);
-router.route("/favorite/:id").get(userAuth, favorite);
-router.route("/unfavorite/:id").get(userAuth, unfavorite);
-router.route("/feature/:id").get(adminAuth, feature);
-router.route("/unfeature/:id").get(adminAuth, unfeature);
-router.route("/comment/:id").post(userAuth, comment);
-router.route("/comment/:id/edit").post(userAuth, editComment);
-router.route("/comment/:id/delete").delete(userAuth, deleteComment);
+ProjectAPI.route(router,adminAuth,userAuth);
+router.route("/favorite/:id").get(userAuth, ProjectAPI.favorite);
+router.route("/unfavorite/:id").get(userAuth, ProjectAPI.unfavorite);
 
 module.exports = router;
