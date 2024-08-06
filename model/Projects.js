fc96a772105ec6a5d19752b98df973f0934819cc
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-05 19:13:50 -0700

    Update Projects.js

--- model/Projects.js
+++ model/Projects.js
@@ -1,25 +1,6 @@
 const Mongoose = require("mongoose");
 
-const CommentSchema = new Mongoose.Schema({
-  content: {
-    type: String,
-    minlength: 1,
-    required: true,
-  },
-  upvotes: [ String ],
-  postedAt: {
-    type: Number,
-    required: true,
-  },
-  posterId: {
-    type: String,
-    required: true,
-  },
-  poster: {
-    type: String,
-    required: true,
-  }
-});
+const CommentSchema require("./Comments");
 
 const ProjectSchema = new Mongoose.Schema({
   name: {
