0d439ce0b5190a4138c85541ddfea87fca09c2a3
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-05 19:12:16 -0700

    Update route.js

--- Project/route.js
+++ Project/route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const ProjectAPI = new (require("./project"))();
+const Projects = require("../model/Projects")
+const ProjectAPI = new (require("./project"))(Projects);
 const { adminAuth, userAuth } = require("../middleware/auth");
 
 ProjectAPI.route(router,adminAuth,userAuth);
-router.route("/favorite/:id").get(userAuth, ProjectAPI.favorite);
-router.route("/unfavorite/:id").get(userAuth, ProjectAPI.unfavorite);
+router.route("/favorite/:id").get(userAuth, ProjectAPI.favorite.bind(ProjectAPI));
+router.route("/unfavorite/:id").get(userAuth, ProjectAPI.unfavorite.bind(ProjectAPI));
 
 module.exports = router;
