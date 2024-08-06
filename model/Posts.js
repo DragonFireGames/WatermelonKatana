1d4765bd319b969909ffbf2f9d557e20a86908d0
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-05 19:16:40 -0700

    Update Posts.js

--- model/Posts.js
+++ model/Posts.js
@@ -1,1 +1,64 @@
 
+const Mongoose = require("mongoose");
+
+const CommentSchema require("./Comments");
+
+const PostSchema = new Mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+  content: {
+    type: String,
+    default: "",
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  tags: [ String ],
+  views: {
+    type: Number,
+    default: 0,
+  },
+  postedAt: {
+    type: Number,
+    required: true,
+  },
+  posterId: {
+    type: String,
+    required: true,
+  },
+  poster: {
+    type: String,
+    required: true,
+  },
+  /*poster: {
+    type: Mongoose.ObjectId,
+    ref: "user",
+  },*/
+  comments: [ CommentSchema ]
+},{
+  methods: {
+    pack: function() {
+      const container = {};
+      container.name = this.name;
+      container.content = this.content;
+      container.featured = this.featured;
+      container.tags = this.tags || [];
+      container.views = this.views;
+      container.comments = this.comments;
+      container.platform = this.platform;
+      container.postedAt = this.postedAt;
+      container.id = this._id;
+      container.posterId = this.posterId;
+      container.poster = this.poster;
+      return container;
+    }
+  }
+});
+
+const Posts = Mongoose.model("post", PostSchema);
+
+module.exports = Posts;
