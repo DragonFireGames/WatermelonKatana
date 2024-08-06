5f3dee25760c9d4c16fb878500a8fe03d1e5e115
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-05 19:14:13 -0700

    Update Comments.js

--- model/Comments.js
+++ model/Comments.js
@@ -1,1 +1,22 @@
+const CommentSchema = new Mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 1,
+    required: true,
+  },
+  upvotes: [ String ],
+  postedAt: {
+    type: Number,
+    required: true,
+  },
+  posterId: {
+    type: String,
+    required: true,
+  },
+  poster: {
+    type: String,
+    required: true,
+  }
+});
 
+module.exports = CommentSchema;
