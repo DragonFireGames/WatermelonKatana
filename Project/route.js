d00f198b167a6a61426b1d727c3bb503ae668e20
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-07 15:03:06 -0700

    Update route.js

--- Project/route.js
+++ Project/route.js
@@ -6,7 +6,7 @@ const ProjectAPI = require("./project");
 const poster = new ProjectAPI(Projects);
 const { adminAuth, userAuth } = require("../middleware/auth");
 
-poster.route(router,adminAuth,userAuth);
+poster.route(router,userAuth,adminAuth);
 router.route("/favorite/:id").get(userAuth, poster.favorite.bind(poster));
 router.route("/unfavorite/:id").get(userAuth, poster.unfavorite.bind(poster));
 
