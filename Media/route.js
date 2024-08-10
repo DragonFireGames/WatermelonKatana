b6413997b85e00139e47a153008e51c60c077afe
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-10 10:33:45 -0700

    Update and rename route.js to route.js

---
+++ Media/route.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require("cross-fetch");
+
+const Media = require("../model/Media");
+const { adminAuth, userAuth } = require("../middleware/auth");
+
+router.route("/upload").post(userAuth, async (req,res) => {
+  try {
+    const data = await fetch("https://api.imgbb.com/1/upload?key="+process.env.IMGBB_API_KEY,{
+      method: "POST",
+      headers: req.headers,
+      body: req.body
+    }).then(r=>r.json());
+    if (data.response.error) throw data.response.error;
+    const image = data.response.data;
+    const user = res.locals.userToken;
+    const media = Media.create({
+      name: image.title,
+      url: image.url,
+      width: image.width,
+      height: image.height,
+      size: image.size,
+      type: image.image.extension,
+      uploadedAt: image.time,
+      poster: user.username,
+      posterId: user.id,
+    });
+    console.log(media);
+    res.status(200).json({
+      message: "Media successfully uploaded",
+      id: media._id,
+      name: media.name,
+    });
+  } catch(error) {
+    res.status(400).json({
+      message: "Media not successfully uploaded",
+      error: error.message,
+    });
+    console.log(error.message);
+  }
+});
+router.route("/get/:id").get(async (req,res) => {
+  try {
+    var mid = req.params.id;
+    var media = await Media.findOne({ _id: mid });
+    var request = await fetch(media.url);
+    if (request.status >= 206) throw { message: "Could not fetch; Error Code: "+request.status };
+    var blob = await request.blob();
+    var buffer = await blob.arrayBuffer();
+    buffer = Buffer.from(buffer);
+    res.set("Content-Type", blob.type);
+    res.send(buffer);
+  } catch(error) {
+    res.status(400).json({
+      message: "Media not successfully fetched",
+      error: error.message,
+    });
+    console.log(error.message);
+  }
+});
+
+module.exports = router;
