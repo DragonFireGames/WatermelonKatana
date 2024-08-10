f887ec0eeb03fe70911de1f673331b7f280cd46d
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-10 15:34:18 -0700

    Update mediaupload.js

--- client/scripts/mediaupload.js
+++ client/scripts/mediaupload.js
@@ -14,24 +14,31 @@
   overflow: show;
   padding: 5px;
   position: absolute;
-  width: 80vmin;
+  width: 72vmin;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   display: none;
 }
 
+#link-insert {
+  margin: 0px 0px 5px 0px;
+  width: calc(72vmin - 10px);
+}
+
 #file-upload {
   margin: 0px 0px 5px 0px;
+  width: calc(72vmin - 10px);
 }
 
 #upload-preview {
-  max-width: 80vmin;
-  max-height: 80vmin;
+  max-width: 72vmin;
+  max-height: 72vmin;
 }
 
 </style>
 <div id="upload-container">
+  <input id="link-insert" type="text" onchange="setPreviewLink()"><br>
   <input id="file-upload" type="file" accept="image/*" onchange="setPreview()"><br>
   <img id="upload-preview"><br>
   <button id="file-upload-submit" onclick="uploadMedia()">Upload</button>
@@ -39,15 +46,20 @@
 </div>`;
 })();
 
-function getFileUpload() {
+function getFileUpload(url) {
   var container = document.querySelector("#upload-container");
   container.style.display = "block";
+  var link = document.querySelector('#link-insert');
+  link.value = url||"";
+  setPreviewLink();
   return new Promise((resolve)=>{
     window.onfileupload = resolve;
   });
 }
 async function uploadMedia() {
   var elem = document.querySelector('#file-upload');
+  var link = document.querySelector('#link-insert');
+  if (link.value || !elem.value) return fileUploaded(link.value);
   var file = elem.files[0];
   var buf = await file.arrayBuffer();
   var b64 = _arrayBufferToBase64(buf);
@@ -65,10 +77,11 @@ async function uploadMedia() {
   } catch (error) {
     alert(JSON.stringify(error));
     console.log(error);
-    fileUploaded(null);
+    fileUploadCancel();
   }
 }
 async function setPreview() {
+  var link = document.querySelector('#link-insert');
   var elem = document.querySelector('#file-upload');
   var img = document.querySelector('#upload-preview');
   
@@ -78,6 +91,14 @@ async function setPreview() {
   var b64 = _arrayBufferToBase64(buf);
   var url = "data:"+file.type+";base64,"+b64;
   img.src = url;
+  link.value = "";
+}
+async function setPreviewLink() {
+  var link = document.querySelector('#link-insert');
+  var elem = document.querySelector('#file-upload');
+  var img = document.querySelector('#upload-preview');
+  img.src = link.value;
+  elem.value = "";
 }
 function fileUploaded(url) {
   var container = document.querySelector("#upload-container");
