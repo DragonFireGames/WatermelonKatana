b75353d22a8f6e96669d8fd98ad33719555fd7e2
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-12 21:35:32 -0700

    Update markdowntextbox.js

--- client/scripts/markdowntextbox.js
+++ client/scripts/markdowntextbox.js
@@ -1,7 +1,9 @@
 
-function markdownTextbox(sel) {
+function markdownTextbox(sel,nobr) {
   var markbox = document.querySelector(sel);
-  markbox.outerHTML += `<button type="button" onclick="replaceSelectedText(document.querySelector('${sel}'),t=>'**'+t+'**')">
+  markbox.outerHTML += `
+    ${nobr?"":"<br>"}
+    <button type="button" onclick="replaceSelectedText(document.querySelector('${sel}'),t=>'**'+t+'**')">
       <svg fill="none" viewBox="0 0 24 24" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#707277" d="M5 6C5 4.58579 5 3.87868 5.43934 3.43934C5.87868 3 6.58579 3 8 3H12.5789C15.0206 3 17 5.01472 17 7.5C17 9.98528 15.0206 12 12.5789 12H5V6Z" clip-rule="evenodd" fill-rule="evenodd"></path>
         <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#707277" d="M12.4286 12H13.6667C16.0599 12 18 14.0147 18 16.5C18 18.9853 16.0599 21 13.6667 21H8C6.58579 21 5.87868 21 5.43934 20.5607C5 20.1213 5 19.4142 5 18V12"></path>
