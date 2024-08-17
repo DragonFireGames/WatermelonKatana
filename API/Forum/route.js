27dedb95e2acc483928921ad2ea48c73f2b1fd2a
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-17 11:46:51 -0700

    Update and rename Forum/route.js to API/Forum/route.js

--- API/Forum/route.js
+++ API/Forum/route.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const Posts = require("../model/Posts")
+const Posts = require(".../model/Posts")
 const PostAPI = require("./post");
 const poster = new PostAPI(Posts,"posts");
-const { adminAuth, userAuth } = require("../middleware/auth");
+const { adminAuth, userAuth } = require(".../middleware/auth");
 
 poster.route(router,userAuth,adminAuth);
 
