a4f15a7df58f1488be175b999058153774df1bfa
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-17 11:44:09 -0700

    Update and rename Admin/route.js to API/Admin/route.js

--- API/Admin/route.js
+++ API/Admin/route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { transferProject, resetPassword } = require("./admin");
-const { adminAuth, userAuth, checkAuth } = require("../middleware/auth");
+const { adminAuth, userAuth, checkAuth } = require(".../middleware/auth");
 
 router.route("/transfer").get(adminAuth, transferProject);
 router.route("/resetpassword").get(adminAuth, resetPassword);
