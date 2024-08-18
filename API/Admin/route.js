22510043acf4792383399287b38b74e9319c9cd1
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-17 19:51:25 -0700

    Update route.js

--- API/Admin/route.js
+++ API/Admin/route.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { transferProject, resetPassword } = require("./admin");
+const { transferProject, resetPassword, createReport, listReports } = require("./admin");
 const { adminAuth, userAuth, checkAuth } = require("../../middleware/auth");
 
 router.route("/transfer").get(adminAuth, transferProject);
 router.route("/resetpassword").get(adminAuth, resetPassword);
+router.route("/reports/create").post(userAuth, createReport);
+router.route("/reports/list").get(adminAuth, listReports);
 
 module.exports = router;
