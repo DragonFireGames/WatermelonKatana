ca0016a292693fa14b04f07d774d1b1b0c92265b
Varrience <105261606+Varrience@users.noreply.github.com>
2024-08-25 19:51:10 +0000

    project replay button

--- Assets/scripts/navbar.js
+++ Assets/scripts/navbar.js
@@ -305,6 +305,10 @@ document.addEventListener("DOMContentLoaded", async function() {
       <a class="nav-btn" href="/chat">Chat</a>
       <a class="nav-btn" href="/search">Project Gallery</a>
       <a class="nav-btn" href="/forum">Forum</a>
+      ${window.location.pathname.startsWith("/project") ? `
+      <a class="nav-btn" onclick="l=document.getElementById('game').contentWindow.location; l.href=l.href"> 
+      Replay
+      </a>`: ""}
     </div>
     <div class="navbar-right">
   `;
