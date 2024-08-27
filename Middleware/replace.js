e053efb457e8c7c1fa8474ee40859e16f5649e5d
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-26 23:00:03 -0700

    Update replace.js

--- Middleware/replace.js
+++ Middleware/replace.js
@@ -1,8 +1,8 @@
 
-const Transform = require("stream").Transform
+const Transform = require("stream").Transform;
 const fs = require("fs");
 
-module.exports = function(res, path, callback) {
+module.exports = function(res, path, callback, xml) {
   const replacementTransform = new Transform();
   replacementTransform._transform = function(data, encoding, done) {
     const str = callback(data.toString());
@@ -10,9 +10,9 @@ module.exports = function(res, path, callback) {
     done();
   };
 
-  res.write('<!-- Begin stream -->\n');
+  if (!xml) res.write('<!-- Begin stream -->\n');
   let stream = fs.createReadStream(path)
   stream.pipe(replacementTransform).on('end', () => {
-    res.write('\n<!-- End stream -->');
+    if (!xml) res.write('\n<!-- End stream -->');
   }).pipe(res);
 }
