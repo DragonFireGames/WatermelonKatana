4ee208d68121dff1495252a37e0eff75542b8d8d
DragonFireGames <107079704+DragonFireGames@users.noreply.github.com>
2024-08-28 12:32:23 -0700

    Update markdowntextbox.js

--- Assets/scripts/markdowntextbox.js
+++ Assets/scripts/markdowntextbox.js
@@ -28,7 +28,7 @@ function markdownTextbox(sel,nobr) {
       <path stroke-linecap="round" stroke-width="2.5" stroke="#707277" d="M17.5 7.66667C17.5 5.08934 15.0376 3 12 3C8.96243 3 6.5 5.08934 6.5 7.66667C6.5 8.15279 6.55336 8.59783 6.6668 9M6 16.3333C6 18.9107 8.68629 21 12 21C15.3137 21 18 19.6667 18 16.3333C18 13.9404 16.9693 12.5782 14.9079 12"></path>
       </svg>
     </button>
-    <button type="button" title="Insert Image" onclick="replaceSelectedText(document.querySelector('${sel}'),async()=>{var url = await getFileUpload(); return '![height=100]('+url+')';})">
+    <button type="button" title="Insert Image" onclick="replaceSelectedText(document.querySelector('${sel}'),async(t)=>{var url = await getFileUpload(); if (url) { return t; } return '![height=100]('+url+')';})">
        <svg fill="#707277" viewBox="0 0 1024 1024" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" stroke="#707277" d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zM338 304c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm513.9 437.1a8.11 8.11 0 0 1-5.2 1.9H177.2c-4.4 0-8-3.6-8-8 0-1.9.7-3.7 1.9-5.2l170.3-202c2.8-3.4 7.9-3.8 11.3-1 .3.3.7.6 1 1l99.4 118 158.1-187.5c2.8-3.4 7.9-3.8 11.3-1 .3.3.7.6 1 1l229.6 271.6c2.6 3.3 2.2 8.4-1.2 11.2z"></path>
       </svg>
