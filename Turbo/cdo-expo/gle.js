5d79715fa0fd6192e4aae38c45f5b187f302f607
Varrience <105261606+Varrience@users.noreply.github.com>
2024-10-06 02:50:35 -0400

    fingers crossed

--- Turbo/cdo-expo/gle.js
+++ Turbo/cdo-expo/gle.js
@@ -328,20 +328,19 @@ window.preload = function () {
         let __script = document.createElement("script");
         __script.text = ${JSON.stringify(libraries + json.source)};
         document.body.appendChild(__script);
-    })
-    .catch(err => {
-        throw new Error(err);
+        try { window.draw = draw; } catch (e) {}
+        switch (stage) {
+          case 'preload':
+            if (preload !== window.preload) { preload(); }
+            break;
+          case 'setup':
+            if (setup !== window.setup) { setup(); } else { window.preload = null; }
+              break;
+        }
+    }).catch(err => {
+       throw new Error(err);
     })
 })();
-    try { window.draw = draw; } catch (e) {}
-    switch (stage) {
-      case 'preload':
-        if (preload !== window.preload) { preload(); }
-        break;
-      case 'setup':
-        if (setup !== window.setup) { setup(); } else { window.preload = null; }
-        break;
-    }
   }
   window.wrappedExportedCode = wrappedExportedCode;
   wrappedExportedCode('preload');
